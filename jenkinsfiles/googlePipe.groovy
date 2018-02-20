11c79d4eebcf6e32e6d36e38198f0248d860d5a1
rozborsky <roman.rozborsky@gmail.com>
2018-02-20 16:13:52 +0200

    add reports

--- jenkinsfiles/googlePipe.groovy
+++ jenkinsfiles/googlePipe.groovy
@@ -3,18 +3,9 @@ echo "hey!"
 stage 'Build'
 
 node {
-    sh 'bundle install'
-
-    // build and run tests with coverage
-    sh 'bundle exec rake build spec'
-
-    // Archive the built artifacts
-    archive (includes: 'pkg/*.gem')
-
-    publishHTML(target:[
+    publishHTML([
             allowMissing: false,
             alwaysLinkToLastBuild: false,
-            keepAll: true,
-            reportDir: '',
-            reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: ''])
+            keepAll: false,
+            reportDir: 'target', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: ''])
 }
