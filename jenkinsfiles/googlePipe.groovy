2558efe44577047e97ca2245f9146e5f237029a2
rozborsky <roman.rozborsky@gmail.com>
2018-02-20 16:47:36 +0200

    add reports

--- jenkinsfiles/googlePipe.groovy
+++ jenkinsfiles/googlePipe.groovy
@@ -5,7 +5,8 @@ stage 'Build'
 node {
     checkout scm
 
-    bat "mvn clean install -Dconfig=google_search -Dtests=google_search.xml"
+    bat "mvn clean install -Dconfig=google_search -Dtests=google_search_suite.xml"
+
     publishHTML([
             allowMissing: false,
             alwaysLinkToLastBuild: false,
