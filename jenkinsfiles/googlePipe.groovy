ec84c103e37e27268ecab7297b0418a7295192c7
rozborsky <roman.rozborsky@gmail.com>
2018-02-20 16:18:50 +0200

    add reports

--- jenkinsfiles/googlePipe.groovy
+++ jenkinsfiles/googlePipe.groovy
@@ -3,6 +3,15 @@ echo "hey!"
 stage 'Build'
 
 node {
+
+    sh 'bundle install'
+
+    // build and run tests with coverage
+    sh 'bundle exec rake build spec'
+
+    // Archive the built artifacts
+    archive (includes: 'pkg/*.gem')
+    
     publishHTML([
             allowMissing: false,
             alwaysLinkToLastBuild: false,
