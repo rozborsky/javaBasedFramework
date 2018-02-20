8a88e82844da1251fb22ea90b11864fcc3ab2240
rozborsky <roman.rozborsky@gmail.com>
2018-02-20 16:03:33 +0200

    add reports

--- jenkinsfiles/googlePipe.groovy
+++ jenkinsfiles/googlePipe.groovy
@@ -3,22 +3,10 @@ echo "hey!"
 stage 'Build'
 
 node {
-    // Checkout
-    git 'https://github.com/rozborsky/javaBasedFramework.git'
-
-    // install required bundles
-    sh 'bundle install'
-
-    // build and run tests with coverage
-    sh 'bundle exec rake build spec'
-
-    // Archive the built artifacts
-    archive (includes: 'pkg/*.gem')
-
     publishHTML(
             [allowMissing: false,
              alwaysLinkToLastBuild: false,
              keepAll: true,
-             reportDir: '{$WORKSPACE}/target',
-             reportFiles: '*.html', reportName: 'HTML Report', reportTitles: ''])
+             reportDir: 'target',
+             reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: ''])
 }
